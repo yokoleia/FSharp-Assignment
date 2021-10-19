5b6f68579772992409b5a47a5d862c52451916e0
Rab Dawanincura <r.yokoleia@gmail.com>
2021-10-19 17:43:08 +1000

    init

--- TSPView/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ TSPView/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -111,7 +111,7 @@ namespace TSPView {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TSPView;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TSPView;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
